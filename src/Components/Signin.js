602b5b85b41d3b140ea16473e5f131497a31451d
jay <jay.mauryavanshi2014@gmail.com>
2019-10-23 21:12:59 +0530

    signin feature completed

--- src/Components/Signin.js
+++ src/Components/Signin.js
@@ -9,12 +9,10 @@ export default class Signin extends Component {
                         <div className="form-group">
                             <input type="email" className="form-control" id="inputEmail" aria-describedby="emailHelp" placeholder=" email" />
                         </div>
-                        
                         <div className="form-group">
                             <input type="password" className="form-control" id="inputConfirmPassword" placeholder="confirm password" />
                         </div>
                         <br /> <br />
-
                         <button type="submit" value="sign in" className="btn btn-dark"> SIGN IN </button>
                     </form>
                 </div>
